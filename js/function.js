9047463592edec1bffb7edb08c52796e407b5f4b
richasyal <richasyal@gmail.com>
2019-10-09 17:07:51 +0800

    responsive function

--- js/function.js
+++ js/function.js
@@ -1,11 +1,11 @@
-const mq = window.matchMedia( "(orientation:landscape)" );
-
-if (mq.matches) {
-
+const mqMobile = window.matchMedia("(max-width : 600px)");
+const mqTablet = window.matchMedia("(min-width : 601px) and (max-width : 992px)");
 
 
-}
-
-else {
-
+if (mqMobile.matches) {
+	console.log('mobile');
+} else if (mqTablet.matches) {
+  	console.log('tablet');
+} else {
+  	console.log('desktop');
 }
