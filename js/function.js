32b685a78185b7289e2fb894a980e92d3dd2e0aa
richasyal <richasyal@gmail.com>
2019-10-14 15:36:56 +0800

    updated

--- js/function.js
+++ js/function.js
@@ -1,11 +1,26 @@
+
+//Responsive layouts
+
 const mqMobile = window.matchMedia("(max-width : 600px)");
 const mqTablet = window.matchMedia("(min-width : 601px) and (max-width : 992px)");
 
 
-if (mqMobile.matches) {
-	console.log('mobile');
-} else if (mqTablet.matches) {
-  	console.log('tablet');
-} else {
-  	console.log('desktop');
+// if (mqMobile.matches) {
+// 	console.log('mobile');
+// } else if (mqTablet.matches) {  	
+//   	console.log('tablet');
+// } else {
+//   	console.log('desktop');
+// }
+
+// responsive nav bar
+
+function openNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "nav-content") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav-content";
+  }
 }
+
